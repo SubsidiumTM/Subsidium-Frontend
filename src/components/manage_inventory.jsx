3b797a922fa2508b7bfab13a0f1e0a3e7596fe46
manu-camacho <manu320camacho@gmail.com>
2022-09-28 19:01:19 -0500

    createRoom

--- src/components/manage_inventory.jsx
+++ src/components/manage_inventory.jsx
@@ -2,15 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../App.css"; 
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage, Auth } from 'aws-amplify'; 
-import { Button,
-  Flex,
-  Heading,
-  Image,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from '@aws-amplify/ui-react';
+import { Button, Flex, Heading, Image, Text, TextField, View, withAuthenticator} from '@aws-amplify/ui-react';
 import { listNotes } from "../graphql/queries";
 import {
   createNote as createNoteMutation,
@@ -29,6 +21,8 @@ import {
   updateRoom as updateRoomMutation,
   updateLicence as updateLicenceMutation
 } from "../graphql/mutations";
+
+import createRoom from "./createRoom";
 
 const Manager = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
@@ -103,32 +97,7 @@ const Manager = ({ signOut }) => {
     event.target.reset();
   };
 
-  async function createRoom(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const data = {
-      name: form.get("room_name"),
-      building : form.get("room_building"),
-      proyector : (form.get("proyector") == null) ? false : true,
-      wifi : (form.get("wifi") == null) ? false : true,
-      board : (form.get("board") == null) ? false : true,
-      air_conditioner : (form.get("air_conditioner") == null) ? false : true,
-      ethernet : (form.get("ethernet") == null) ? false : true,
-      computers : (form.get("computers") == null) ? false : true,
-      double_monitor : (form.get("double_monitor") == null) ? false : true,
-      seats : form.get("seats"),
-      energy_outlets : form.get("energy_outlets"),
-      description : form.get("room_description"),
-      images: [""],
-    };
-    // if (!!data.image) await Storage.put(data.name, image);
-    await API.graphql({
-      query: createRoomMutation,
-      variables: { input: data },
-    });
-    // fetchNotes();
-    event.target.reset();
-  }
+  
 
   async function createDevice(event) {
     event.preventDefault();
