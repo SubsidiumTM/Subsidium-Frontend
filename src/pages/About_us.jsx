4b9bb60678215790c4c1ca1b6e9a6e57c875d465
A01655181 <a01655181@tec.mx>
2022-10-06 18:45:01 -0500

    Pagina de about us al 75%

--- src/pages/About_us.jsx
+++ src/pages/About_us.jsx
@@ -1,5 +1,5 @@
+import { Flex } from '@aws-amplify/ui-react';
 import React from 'react'
-//import DirHub from './DirectorHub.png'
 
 function About_us() {
   return (
@@ -19,22 +19,68 @@ function About_us() {
       
       <div>
         <h2> Director del Hub </h2>
-        <img src= "DirectorHub.png"></img>
-        <p> El Tec de Monterrey viene haciendo un trabajo excelente en materia 
-        de ciberseguridad con iniciativa y aliados estratégicos.
-        Me gustaría dar seguimiento a todas las buenas iniciativas y 
-        actividades que se vienen haciendo, las relaciones que se han 
-        desarrollado y fortalecer y desarrollar otras nuevas.
-        Por ejemplo, impulsar al género femenino a adentrarse a la 
-        materia, como saben menos del 10% del sector de ciberseguridad 
-        son mujeres. Debemos poner nuestro grano de arena y apoyar 
-        estas iniciativas </p>
+        <Flex
+          direction= "row"
+          justifyContent= "flex-start" 
+          alignItems= "center">
+
+            <view>
+             <img src= 'https://drive.google.com/file/d/1sj4Tfd39rJyBJJA2Mm6WKQ3r4IIuEyOe/view?usp=sharing'/>
+             <p> Gonzalo García-Belenguer Cuchi </p>
+             <a href="https://www.linkedin.com/in/gonzalo-garc%C3%ADa-belenguer-63968768"> ggarciab@tec.mx </a>
+            </view>
+
+            <view>
+            <p> El Tec de Monterrey viene haciendo un trabajo excelente en materia 
+            de ciberseguridad con iniciativa y aliados estratégicos.
+            Me gustaría dar seguimiento a todas las buenas iniciativas y 
+            actividades que se vienen haciendo, las relaciones que se han 
+            desarrollado y fortalecer y desarrollar otras nuevas.
+            Por ejemplo, impulsar al género femenino a adentrarse a la 
+            materia, como saben menos del 10% del sector de ciberseguridad 
+            son mujeres. Debemos poner nuestro grano de arena y apoyar 
+            estas iniciativas </p>
+            </view>
+
+        </Flex>
       </div>
 
       <div>
         <h2> Nuestros socios </h2>
         <p> IMAGEN VRGAS </p>
       </div>
+
+      <div>
+        <h2> Líneas de trabajo </h2>
+        <p> IMAGEN VRGAS2 </p>
+      </div>
+
+      <div>
+        <h2> En caso de registrarse el Hub ofrece la posibilidad de reservación de los siguientes recursos: </h2>
+        <Flex
+          direction= "row"
+          justifyContent= "flex-start" 
+          alignItems= "center">
+
+            <view>
+              <p> Imagen1 </p>
+              <p> Imagen2 </p>
+              <p> Imagen3 </p>
+            </view>
+
+            <view>
+              <p> Ofrecemos la posibilidad de reservar espacios con ciertas características para promover el trabajo y el aprendizaje. </p>
+              <p> A los miembros se les ofrece la posibilidad de poder reservar equipos de cómputo para promover el aprendizaje. </p>
+              <p> En caso de ser necesario, se ofrece la posibilidad de utilizar licencias de software establecidas por cierto tiempo. </p>
+            </view>
+        </Flex>
+      </div>
+
+      <footer>
+        <div>
+          <p> footer mamalon</p>
+        </div>
+      </footer>
     </div>
   )
 }
