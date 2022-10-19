42cc5bf9405e0d689ed25fdef4f99b78c5ffc187
A01655181 <a01655181@tec.mx>
2022-10-18 21:55:12 -0500

    Estilos listos

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import SubsidiumAuth from '../components/AuthenticationWrap'
 import Profile_menu from '../components/profile_menu'
+import '../components/profileIntro.css'
 
 function Profile() {
   return (
 
+    <body className='profileBack'>
     <SubsidiumAuth jsx={
 
     <Profile_menu />
 
     }/>
+    </body>
 
   )
 }
